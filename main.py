4d1992118db263c24562598b8c33b616e4fcd6e3
bhartisaurav <33656540+bhartisaurav@users.noreply.github.com>
2022-05-15 13:02:15 +0530

    add a line for read from the excel file

--- main.py
+++ main.py
@@ -10,9 +10,22 @@ loc = "/home/saurav/Documents/setReminderFile.xls"
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 
-for c in range(0, sheet.ncols):
-    l = sheet.col_values(c, 1, sheet.nrows)
-    print("list: ", l)
+list_of_start_time_of_each_reminder = []
+list_of_reminder_time = sheet.col_values(1, 1, sheet.nrows)
+
+for i in range(0, len(list_of_reminder_time)):
+    reminder_time = list_of_reminder_time[i]
+    if len(list_of_start_time_of_each_reminder)-1 >=i and list_of_start_time_of_each_reminder[i]:
+        # convert the reminder time into timeout
+        timeout = get_timeout_time(reminder_time)
+    else:
+        list_of_start_time_of_each_reminder.
+
+
+    times_loop_run = sheet.col_values(0, i, )
+    notification.notify(
+
+    )
 #TODO: get the time from the excel sheet and print its description and run in background.
 #TODO:
 #print(sheet.cell_value(0, 0))
